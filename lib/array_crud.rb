2d075612df29fff84f44479249936f23be8b524d
Kyshon Gary <kgarywork21@gmail.com>
2019-12-18 05:09:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  my_array = ["earth", "wind", "fire", "water"]
-  my_array << "solid"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
