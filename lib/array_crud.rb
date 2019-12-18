cfd3d248702e684552c4cd5a2b72450ac55a695d
Kyshon Gary <kgarywork21@gmail.com>
2019-12-18 05:26:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,9 +30,6 @@ def retrieve_first_element_from_array(array)
   array[0]
 end
 
-def retrieve_last_element_from_array
-  array[-1]
-end
 def retrieve_last_element_from_array(array)
-  
+ array[-1] 
 end
